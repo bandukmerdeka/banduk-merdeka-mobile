9c6d4adb99217c62cde93fe360f831485e325b03
Giansar Pratiknya <gpratiknya@gmail.com>
2015-10-25 11:23:03 +0700

    commit

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -18,6 +18,7 @@
  */
 var map;
 var mapClickable;
+var datadetail = [];
 var app = {
     // Application Constructor
     initialize: function () {
@@ -57,11 +58,13 @@ var app = {
             .factory('provinceService', ['$resource', provinceService])
             .factory('categoryService', ['$resource', categoryService])
             .factory('submittedReportService', ['$resource', submittedReportService])
+            .factory('submittedReportServiceId', ['$resource', submittedReportServiceId])
             .controller('FrontController', ['$location', FrontController])
             .controller('CreateReportController', ['provinceService', 'categoryService', 'submittedReportService', '$location', CreateReportController])
             .controller('MapController', ['$location', MapController])
             .controller('NavController', ['$mdSidenav', NavController])
-            .controller('ReportListController', ['submittedReportService', '$location', ReportListController])
+            .controller('ReportListController', ['submittedReportService', 'submittedReportServiceId', '$location', ReportListController])
+            .controller('ReportDetailController', ['categoryService', '$location', ReportDetailController])
             .controller('FormPersonRegistrationController', ['provinceService', FormPersonRegistrationController]);
 
         angular.element(document).ready(function () {
@@ -85,6 +88,11 @@ var app = {
                     controller: 'ReportListController',
                     controllerAs: 'reportList'
                 })
+                .when('/detail', {
+                    templateUrl: 'detailReport.html',
+                    controller: 'ReportDetailController',
+                    controllerAs: 'reportDetail'
+                })
                 .when('/peta', {
                     templateUrl: 'map.html',
                     controller: 'MapController',
@@ -106,6 +114,11 @@ var app = {
             //return $resource('http://localhost:3000/api/submittedReport', null, {})
         }
 
+        function submittedReportServiceId($resource) {
+            return $resource('http://bandukrelawanapi-gpratiknya.rhcloud.com/api/submittedReport/:id', null, {})
+            //return $resource('http://localhost:3000/api/submittedReport', null, {})
+        }
+
         function FrontController($location) {
             console.log('FrontController');
             var vm = this;
@@ -114,19 +127,110 @@ var app = {
             };
         }
 
-        function ReportListController(submittedReportService, $location) {
+        function ReportDetailController(categoryService, $location) {
+            var vm = this;
+
+            vm.gotoPage = function (path) {
+                console.log(path);
+                $location.path(path);
+            };
+
+            vm.showButtonMap = true;
+            vm.province = datadetail.province;
+            vm.city = datadetail.city;
+            vm.kecamatan = datadetail.kecamatan;
+            vm.kelurahan = datadetail.kelurahan;
+            vm.name = datadetail.name;
+            vm.address = datadetail.address;
+            vm.description = datadetail.description;
+
+            vm.categories = [];
+            var result = categoryService.get(
+                {},
+                function () {
+                    vm.categories = result.data;
+                });
+
+            vm.openMap = function () {
+                var div = document.getElementById("map_canvasi");
+                var aPlace = new plugin.google.maps.LatLng(datadetail.latitude, datadetail.longitude);
+                map = plugin.google.maps.Map.getMap(div, {
+                    'camera': {
+                        'latLng': aPlace,
+                        'zoom': 12
+                    }
+                });
+                map.setCenter(aPlace);
+                map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
+                map.addMarker({
+                        'position': aPlace
+                    }, function (marker) {
+                        marker.setTitle(aPlace.toUrlValue());
+                                marker.showInfoWindow();
+                    });
+                });
+                //getCurrentPosition();
+                vm.showButtonMap = false;
+
+                function getCurrentPosition() {
+                    var onSuccess = function (position) {
+                        mapGotoCurrentPosition(position, true);
+                    };
+                    function onError(error) {
+                        console.log('onError');
+                        alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n')
+                    };
+                    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+                }
+
+                function mapGotoCurrentPosition(position, isDraggable) {
+                    vm.latitude = position.coords.latitude;
+                    vm.longitude = position.coords.longitude;
+                    var myPosition = new plugin.google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                    map.addMarker({
+                        'position': myPosition,
+                        'draggable': isDraggable
+                    }, function (marker) {
+                        map.setCenter(myPosition);
+                        marker.addEventListener(plugin.google.maps.event.MARKER_DRAG_END, function (marker) {
+                            marker.getPosition(function (latLng) {
+                                marker.setTitle(latLng.toUrlValue());
+                                marker.showInfoWindow();
+                                vm.latitude = latLng.lat;
+                                vm.longitude = latLng.lng;
+                            });
+                        });
+                    });
+                }
+            };
+        }
+
+        function ReportListController(submittedReportService, submittedReportServiceId, $location) {
             console.log('ReportListController');
             var vm = this;
-            vm.submittedReports;
+            vm.submittedReports = [];
+            vm.submittedReport = [];
             var result = submittedReportService.get(
                 {},
                 function () {
                     vm.submittedReports = result.data;
                 });
+
             console.log(vm.submittedReports);
+
             vm.gotoPage = function (path) {
                 $location.path(path);
             };
+
+            vm.getDetail = function (id) {
+                console.log('getDetail=' + id);
+                var result = submittedReportServiceId.get(
+                    { id: id },
+                    function () {
+                        datadetail = result.data;
+                        $location.path('/detail');
+                    });
+            }
         }
 
         function CreateReportController(provinceService, categoryService, submittedReportService, $location) {
@@ -136,6 +240,15 @@ var app = {
             vm.showButtonMap = true;
             vm.provinces = [];
             vm.categories = [];
+            
+            //default values for demo only
+            vm.province = "Jawa Barat";
+            vm.city = "Bandung";
+            vm.kecamatan = "Kecamatan A";
+            vm.kelurahan = "Kelurahan B";
+            vm.name = "Parmin";
+            vm.address = "jl. kemuliaan";
+            vm.description = "Belum terdata sebagai penduduk kurang mampu. Sedang sakit dan tidak mampu berobat.";
 
             vm.getProvinces = function () {
                 console.log('getProvinces');
@@ -160,6 +273,7 @@ var app = {
                     angular.toJson(vm),
                     function () {
                         console.log('success');
+                        vm.isSaved = 'Berhasil';
                     },
                     function () {
                         console.log('error');
